f77c56154cdf003daaee8eb7ced4778970fb6b87
anthonyd43 <60870614+anthonyd43@users.noreply.github.com>
2022-07-28 16:29:19 -0700

    Implemented Feedback Cards

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import Header from "./Components/Header"
-import FeedBackItem from "./Components/FeedbackItem"
+import FeedbackData from "./data/FeedbackData"
+import FeedbackList from "./Components/FeedbackList"
+import { useState } from "react"
 
 function App() {
+  const [feedback, setFeedback] = useState(FeedbackData)
+
   return (
     <>
       <Header />
       <div className="container">
-        <FeedBackItem/>
+        <FeedbackList feedback={feedback} />
       </div>
     </>
   )
---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident odit consequatur non modi quo dolores!",
+  },
+  {
+    id: 2,
+    rating: 9,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, neque.",
+  },
+  {
+    id: 3,
+    rating: 8,
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil amet vitae nisi aperiam fuga quod, sapiente voluptate ea, ab incidunt autem.",
+  },
+]
+
+export default FeedbackData
