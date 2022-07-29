63fa810f3bd3ce4638415950e8736b393d7de367
anthonyd43 <60870614+anthonyd43@users.noreply.github.com>
2022-07-28 18:04:08 -0700

    Implemented FeedbackForm/List/Stats

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
+import { v4 as uuidv4 } from "uuid"
 import Header from "./Components/Header"
 import FeedbackData from "./data/FeedbackData"
 import FeedbackList from "./Components/FeedbackList"
+import FeedbackStats from "./Components/FeedbackStats"
+import FeedbackForm from "./Components/FeedbackForm"
 import { useState } from "react"
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData)
 
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4()
+    setFeedback([newFeedback, ...feedback])
+  }
+
+  const deleteFeedback = (id) => {
+    if (window.confirm("Are you sure you want to delete?")) {
+      setFeedback(feedback.filter((item) => item.id !== id))
+    }
+  }
+
   return (
     <>
       <Header />
       <div className="container">
-        <FeedbackList feedback={feedback} />
+        <FeedbackForm handleAdd={addFeedback} />
+        <FeedbackStats feedback={feedback} />
+        <FeedbackList
+          feedback={feedback}
+          // Prop Drilling
+          handleDelete={deleteFeedback}
+        />
       </div>
     </>
   )
